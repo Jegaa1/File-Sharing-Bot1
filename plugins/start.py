4e7474c20200cc24fc919b9bd143f5bcb5a04a8f
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-03-23 10:09:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🗑️ Close", callback_data = "close")
                 ]
             ]
         )
