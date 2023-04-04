7392dc169a88de328ab528135a3aa610a61edca3
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-04-04 18:07:15 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -3,7 +3,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, SITE, API
 from helper_func import encode, get_message_id
 from short import get_shortlink
 
