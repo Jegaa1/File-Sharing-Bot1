50baefbd6e79e29fccde004cbf5f6a3e8e9ca5ea
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-08-09 23:13:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "leecher")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "73a4c60a64ca6c75279d14238fd85cc9e779c2ad")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 28800)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "True") == "TRUE" else False
 TUT_VID = os.environ.get("TUT_VID","https://t.me/demoshort/55")
 
 
